e16c749b1275e5cc1c51a700c45c364516cd43d5
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2021-04-20 17:54:47 +0300

    registration completed

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var start_time;
 var time_elapsed;
 var interval;
 
+
+
+
 $(document).ready(function() {
 	context = canvas.getContext("2d");
 	Start();
@@ -191,3 +194,113 @@ function Hide_Display() {
 	$('#register').hide();
 	$('#login').hide();
 };
+
+
+let usersDB = [
+	{
+		username: "k",
+		password: "k",
+		FullName: "k",
+		Email: "k@k.com",
+		Birthday: "3/3/2012"
+	},
+	{
+		username: "avichaiedri1",
+		password: "isTheBoss ",
+		FullName: "k",
+		Email: "k@k.com",
+		Birthday: "3/3/2012"
+	},
+	{
+		username: "tomrob123",
+		password: "isAtLoss",
+		FullName: "k",
+		Email: "k@k.com",
+		Birthday: "3/3/2012"
+	}
+];
+
+
+
+
+$(document).ready(function ()
+{
+	
+	$.validator.addMethod("validPassword", function(value,element,usernameAttribute) {
+		return usernameAttribute.val() !== "" && (value === "k" || (value.length >= 6 && hasNumberAndLetter(value)));
+	}, "Please re-enter a valid password, with at least one number and one letter, and over 5 characters");
+
+	$.validator.addMethod("validFullName", function(value) {
+		return hasOnlyLetters(value);
+	}, "Please re-enter a valid name, with only letters.");
+
+
+	$(function(){
+		$("#form_registration_id").validate({
+			rules: {
+				Username: {
+					required: true,
+				},
+				Password: {
+					required: true,
+					validPassword: $("#RegUsername")
+
+				},
+				FullName: {
+					required: true, 
+					validFullName: true
+				},
+				Email: {
+					required: true, 
+					email: true
+				},
+
+
+				Birthday: {
+					required: true 
+				}
+			},
+			submitHandler: function() {
+				handleNewClient();
+			}
+		});
+	});	
+});
+
+
+
+function hasNumberAndLetter(myString) {
+	let regex = /[a-zA-Z]\d/g;
+	return regex.test(myString);
+  }
+
+function hasOnlyLetters(myString){
+	let regex = /^[a-zA-Z]+$/;
+	return regex.test(myString);
+
+} 
+function getRecord(recordID){
+	return usersDB.find( ({ username }) => username === recordID );
+}
+
+
+function handleNewUser(){
+	usersDB.push(
+		{
+		username: $("#RegUsername").val(),
+		password: $("#RegPassword").val(),
+		FullName: $("#RegFullName").val(),
+		Email: $("#RegEmail").val(),
+		Birthday: $("#RegBirthday").val()
+		}	
+	);
+	$('#form_registration_id').trigger("reset");
+	// TODO add screen switch to login
+	// console.log(usersDB[3]);
+}
+
+// LOGIN 
+//getRecord(username).password === value
+//	$.validator.addMethod("validUserName", function(value) {
+	// return typeof getRecord(value) !== "undefined";
+// }, "Please enter a valid username");
